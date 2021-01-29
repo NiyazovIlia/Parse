74ef339a9a8ca38c0384fd3c6bec237e29c311e0
IliaNiyazof <niazovilya@yandex.ru>
2021-01-29 23:40:40 +0300

    lesson-4

---
+++ gb_parse/spiders/autoyoula.py
@@ -0,0 +1,55 @@
+import pymongo
+import scrapy
+import bs4
+import requests
+
+
+class AutoyoulaSpider(scrapy.Spider):
+    name = "autoyoula"
+    allowed_domains = ["auto.youla.ru"]
+    start_urls = ["https://auto.youla.ru/"]
+
+    css_query = {
+        "brands": "div.ColumnItemList_container__5gTrc a.blackLink",
+        "pagination": "div.Paginator_block__2XAPy a.Paginator_button__u1e7D",
+        "ads": "article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu",
+    }
+
+    @staticmethod
+    def gen_task(response, link_list, callback):
+        for link in link_list:
+            yield response.follow(link.attrib["href"], callback=callback)
+
+    data_query = {
+        "title": lambda resp: resp.css("div.AdvertCard_advertTitle__1S1Ak::text").get(),
+        'img': lambda resp: resp.css(
+            'figure.PhotoGallery_photo__36e_r img.PhotoGallery_photoImage__2mHGn').attrib.get('src'),
+        "price": lambda resp: float(resp.css('div.AdvertCard_price__3dDCr::text').get().replace("\u2009", '')),
+        'description': (lambda resp: resp.css('div.AdvertSpecs_data__xK2Qx a.blackLink::text').get()) or
+                       (lambda resp: resp.css('div.AdvertSpecs_data__xK2Qx::text').get()),
+        # 'author':
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_client = pymongo.MongoClient()
+
+    def parse(self, response, **kwargs):
+        brands_links = response.css(self.css_query["brands"])
+        yield from self.gen_task(response, brands_links, self.brand_parse)
+
+    def brand_parse(self, response):
+        pagination_links = response.css(self.css_query["pagination"])
+        yield from self.gen_task(response, pagination_links, self.brand_parse)
+        ads_links = response.css(self.css_query["ads"])
+        yield from self.gen_task(response, ads_links, self.ads_parse)
+
+    def ads_parse(self, response):
+        # response - ссылка
+        data = {}
+        for key, selector in self.data_query.items():
+            try:
+                data[key] = selector(response)
+            except (ValueError, AttributeError):
+                continue
+        self.db_client['gb_parse_youla'][self.name].insert_one(data)
