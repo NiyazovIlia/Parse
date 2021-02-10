114e0c466e5adc71d793b3288ace20e281fbc4c2
IliaNiyazof <niazovilya@yandex.ru>
2021-02-10 19:57:36 +0300

    lesson-7

---
+++ gb_parse/items.py
@@ -0,0 +1,53 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+# тут храним все data которые должны быть
+
+import scrapy
+
+
+class GbParseItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class AutoyoulaItem(scrapy.Item):
+    _id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    images = scrapy.Field()
+    description = scrapy.Field()
+    author = scrapy.Field()
+    specification = scrapy.Field()
+    price = scrapy.Field()
+
+
+class Insta(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    img = scrapy.Field()
+
+
+class InstaTag(Insta):
+    pass
+
+
+class InstaPost(Insta):
+    pass
+
+
+class InstaUser(Insta):
+    pass
+
+
+# кто подписан на него
+class InstaFollowers(Insta):
+    pass
+
+
+# на кого подписан
+class InstaFollowing(Insta):
+    pass
